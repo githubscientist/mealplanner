c6138bce933316311c1fbc017c36754bdd59fdcb
githubscientist <sathish.datascientist@gmail.com>
2025-09-14 13:03:47 +0530

    register, login, auth middleware

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 // import express module
 const express = require('express');
 const mealplanRouter = require('./routes/mealplanRoutes');
+const authRouter = require('./routes/authRoutes');
 
 // create an express application
 const app = express();
 
 app.use(express.json());
 
+app.use('/auth', authRouter);
 app.use('/mealplans', mealplanRouter);
 
 module.exports = app;
