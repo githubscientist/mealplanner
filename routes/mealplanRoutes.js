72ad6b108ce7c05c155d8dfcb22aa5ca2f245595
githubscientist <sathish.datascientist@gmail.com>
2025-09-14 10:44:46 +0530

    get meal plan by id --done

--- routes/mealplanRoutes.js
+++ routes/mealplanRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { getAllMealPlans, createMealPlan } = require('../controllers/mealplanController');
+const { getAllMealPlans, createMealPlan, getMealPlanById } = require('../controllers/mealplanController');
 const mealplanRouter = express.Router();
 
 mealplanRouter.post('/', createMealPlan);
 mealplanRouter.get('/', getAllMealPlans);
+mealplanRouter.get('/:id', getMealPlanById);
 
 module.exports = mealplanRouter;
